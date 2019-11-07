3b4523f3e69bdb695eb919f652de158d0a9f75b6
Alex Bannow <rbannal@gmail.com>
2019-11-07 13:47:00 -0500

    created database seed file

--- src/seeds/seed.bookmarks.sql
+++ src/seeds/seed.bookmarks.sql
@@ -1,2 +1,18 @@
 INSERT INTO bookmarks (title, url, description, rating)
 VALUES
+('clown school', 'clownschool.com', 'for clowns', 5),
+('dog school', 'dogschool.com', 'for dogs', 5),
+('art school', 'artschool.com', 'for art', 3),
+('car school', 'carschool.com', 'for cars', 2),
+('beer school', 'beerschool.com', 'for beer', 5),
+('cat school', 'catschool.com', 'for cats', 4),
+('school school', 'schoolschool.com', 'for schools', 1),
+('juggling school', 'jugglingschool.com', 'for juggling', 2),
+('music school', 'musicschool.com', 'for music', 3),
+('boat school', 'boatschool.com', 'for boats', 2),
+('tree school', 'treeschool.com', 'for trees', 5),
+('harp school', 'harpschool.com', 'for harps', 3),
+('computer school', 'computerschool.com', 'for computers', 4),
+('error school', 'errorschool.com', 'for errors', 5),
+('post school', 'postschool.com', 'for posts', 2),
+('jumping school', 'jumpingschool.com', 'for jumping', 1)
